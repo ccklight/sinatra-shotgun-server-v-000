5a2fc9af00eef7be9624a8e8805b6ee872f4a9ee
Cheryl K <cherylkelson@icloud.com>
2019-02-20 18:57:22 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,13 +2,8 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
-get '/' do
-  "Will this appear?"
-end
-
-
   get '/' do
-      "Welcome to your app!!!! I BUILT THIS!"
-    end
+    "Started my server using Shotgun!"
+  end
 
 end
